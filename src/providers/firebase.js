b81526c630ade91cb2bc6e310c1183018f80eaf7
Miguel Escurra <miguelescurra@gmail.com>
2025-12-14 19:44:20 -0300

    Variables de entorno aplicadas

--- src/providers/firebase.js
+++ src/providers/firebase.js
@@ -1,21 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Validación básica de config
-for (const [k, v] of Object.entries(firebaseConfig)) {
+/* for (const [k, v] of Object.entries(firebaseConfig)) {
   if (!v) {
     console.warn(`[WARN] Variable Firebase faltante: ${k}. Revisa tu .env`);
   }
-}
+} */
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
